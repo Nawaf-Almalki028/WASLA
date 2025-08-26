d4fc011948508f4d3dcdc53ae9df96f4350be7c2
Nawaf Kamal <xp.mo91@gmail.com>
2025-08-26 11:18:31 +0300

    css for apps

--- WaslaProject/WaslaProject/urls.py
+++ WaslaProject/WaslaProject/urls.py
@@ -1,10 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from . import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('dashboard/', include("dashboard.urls")),
     path('accounting/', include("accounting.urls")),
     path('', include("main.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
