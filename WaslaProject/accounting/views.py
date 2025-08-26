2e1e7607185a45547f32764e65a038e976be9a24
Nawaf Kamal <xp.mo91@gmail.com>
2025-08-26 14:18:16 +0300

    update media pages

--- WaslaProject/accounting/views.py
+++ WaslaProject/accounting/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render,redirect
 from django.http import HttpRequest,HttpResponse
 
 
-def accounting_test(request:HttpRequest):
-    return render(request, 'main/accounting.html')
+def accounting_signin(request:HttpRequest):
+    return render(request, 'main/signin.html')
+
+def accounting_signup(request:HttpRequest):
+    return render(request, 'main/signup.html')
+
+def accounting_profile(request:HttpRequest):
+    return render(request, 'main/profile.html')
+
+def accounting_edit_profile(request:HttpRequest):
+    return render(request, 'main/edit_profile.html')
