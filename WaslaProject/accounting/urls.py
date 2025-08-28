e56b6295d982921981ba0592fe03f1976ade5263
Moh0py <1vipmoh@gmail.com>
2025-08-28 17:39:10 +0300

    Support add all pages

--- WaslaProject/accounting/urls.py
+++ WaslaProject/accounting/urls.py
@@ -10,9 +10,6 @@ urlpatterns = [
     path('profile/', views.accounting_profile, name='accounting_profile'),
     path('edit_profile/', views.accounting_edit_profile, name='accounting_edit_profile'),
     path('logout/', views.accounting_logout, name='accounting_logout'),
-    path('term/',views.term, name='terms'),
-    path('FQ',views.FQ_view, name='FQ'),
-    path('feedback/',views.feedback, name='feedback'),
-    path('contact',views.contact, name='contact'),
+    
 ]
 
