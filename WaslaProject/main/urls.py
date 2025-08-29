67ba82fe0325ecb3ad4d3c4ec792bff062b1356c
Hassan Abdullah <hassan8abdulla@gmail.com>
2025-08-29 19:34:27 +0300

    connect main templates with models

--- WaslaProject/main/urls.py
+++ WaslaProject/main/urls.py
@@ -6,10 +6,11 @@ app_name = 'main'
 urlpatterns = [
     path('base/', views.base_view, name="base_view"),
     path('', views.home_view, name='home_view'),
-    path('hackathon_details/', views.hackathon_details_view, name='hackathon_details_view'),
+    path('hackathon_details/<hackathon_id>/', views.hackathon_details_view, name='hackathon_details_view'),
     path('all_hackathons/', views.all_hackathons_view, name='all_hackathons_view'),
     path('pricing/', views.pricing_view, name='pricing_view'),
     path('teams/', views.teams_view, name='teams_view'),
+    path('create_team/', views.create_team_view, name='create_team_view'),
 
 
 
