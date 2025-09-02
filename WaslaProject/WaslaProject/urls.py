aad8ca0fb89dbb5f5378fa480021f8d5810263a3
Nawaf-Almalki028 <xp.mo91@gmail.com>
2025-09-02 09:37:04 +0300

    update accounting

--- WaslaProject/WaslaProject/urls.py
+++ WaslaProject/WaslaProject/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('dashboard/', include("dashboard.urls")),
     path('accounting/', include("accounting.urls")),
+    path('accounts/', include("accounting.urls")),
     path('', include("main.urls")),
     path('support/', include("supportPlus.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)#
