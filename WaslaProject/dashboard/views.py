f0b4b6228de42afc06bd5e46de8035e8eaf3092a
meshari alharbi <109522810+alharbisa24@users.noreply.github.com>
2025-09-03 11:46:26 +0300

    delete note,fixes

--- WaslaProject/dashboard/views.py
+++ WaslaProject/dashboard/views.py
@@ -1276,3 +1276,24 @@ def dashboard_add_requirement_view(request:HttpRequest, hackathon_id:int):
     messages.error(request, "Invalid request method.", "bg-red-600")
     return redirect(request.META.get("HTTP_REFERER", "dashboard:dashboard_hackathons_view"))
 
+
+
+def delete_judge_note_view(request:HttpRequest, note_id:int):
+    if not request.user.is_authenticated or not request.user.user_profile.account_type == 'organization':
+        return redirect("accounting:accounting_signin")
+
+    try:
+        judge_note = models.JudgeNote.objects.get(pk=note_id)
+        if not judge_note.judge.hackathon.organization == request.user:
+            messages.error(request, f"Sorry ! you cannot access this page","bg-red-600")
+            redirect_url = request.META.get("HTTP_REFERER", "dashboard:dashboard_hackathons_view")
+            return redirect(f'{redirect_url}') 
+
+        judge_note.delete()
+        messages.success(request, "Judge note deleted successfully.", "bg-green-600")
+        return redirect(request.META.get("HTTP_REFERER", "dashboard:dashboard_hackathons_view"))
+
+    except models.JudgeNote.DoesNotExist:
+        messages.error(request, "Judge note not found.", "bg-red-600")
+        return redirect(request.META.get("HTTP_REFERER", "dashboard:dashboard_hackathons_view"))        
+    
